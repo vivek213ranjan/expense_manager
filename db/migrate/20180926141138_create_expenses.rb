72cc6f491ebfe34d37ec90f5ed838336b35d588d
Vivek <ranjanvivek1994@gmail.com>
2018-09-26 19:43:15 +0530

    initialized repo with expense model

---
+++ db/migrate/20180926141138_create_expenses.rb
@@ -0,0 +1,11 @@
+class CreateExpenses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :expenses do |t|
+      t.string :category
+      t.double :amount
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
